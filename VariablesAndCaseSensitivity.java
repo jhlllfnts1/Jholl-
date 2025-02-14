5d6bb7d7f107a398f44cf600e7547f84b767f67b
jhlllfnts1 <fuentesjholl_bscs@plmun.edu.ph>
2025-02-14 21:53:23 +0800

    git commit -m "Submitting OBJPROG Week 02 - Session 01 - Exercise 01"

---
+++ VariablesAndCaseSensitivity.java
@@ -0,0 +1,15 @@
+public class VariablesAndCaseSensitivity {
+    public static void main(String[] args) {
+
+int studentAge = 15;
+double itemPrice = 29.99;
+String studentName = "Alice";
+int counter = 10;
+int Counter = 20;
+System.out.println("Student Age: " + studentAge);
+System.out.println("Item Price: $" + itemPrice);
+System.out.println("Student Name: " + studentName);
+System.out.println("Counter: " + counter);
+System.out.println("Counter (Different Case): " + Counter);
+    }
+}
